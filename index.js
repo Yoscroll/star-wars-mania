0b0e89b47d015028b58cca0766234d241957dfc6
Sen2017 <casintahan47at@gmail.com>
2017-09-15 10:46:04 -1000

    fixed display syntax

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let getFilmData = (id)=> $.get( "https://swapi.co/api/films/"+id+"/", (data)=> buildHtmlElements(data));
+let getFilmData = (id)=> $.get( "https://swapi.co/api/films/"+id+"/", (data)=> buildHtmlElements(data) );
 //let buildHtmlElements = (responseData) => 
 console.log("here");
 /*function getFilmData(id) {
@@ -10,14 +10,14 @@ console.log("here");
 function buildHtmlElements(responseData){
 	var title = responseData.title;
   	var director = responseData.director;
-  	var episode = responseData.episode;
+  	var episode = responseData.episode_id;
   	var producer = responseData.producer;
   	var opening_crawl = responseData.opening_crawl;
   	var release_date = responseData.release_date;
   	$('<h2 class="film-title">'+title+'</h2>').appendTo($("#films"));
-  	$('<p class="film-director">'+director+'</p>').appendTo($("#films"));
-  	$('<p class="film-episode">'+episode+'</p>').appendTo($("#films"));
-  	$('<p class="film-producer">'+producer+'</p>').appendTo($("#films"));
-  	$('<p class="film-opening_crawl">'+opening_crawl+'</p>').appendTo($("#films"));
-  	$('<p class="film-release_date">'+release_date+'</p>').appendTo($("#films"));
+  	$('<p class="film-director">Directed by: '+director+'</p>').appendTo($("#films"));
+  	$('<p class="film-episode">Episode #'+episode+'</p>').appendTo($("#films"));
+  	$('<p class="film-producer">Produced by: '+producer+'</p>').appendTo($("#films"));
+  	$('<p class="film-opening_crawl"> Description:'+opening_crawl+'</p>').appendTo($("#films"));
+  	$('<p class="film-release_date">Relase date:'+release_date+'</p>').appendTo($("#films"));
 }
