7522cd6c6adbe90534fd9cf04532769970cab086
Sen2017 <casintahan47at@gmail.com>
2017-09-15 10:39:26 -1000

    added build elm

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-//let getFilmData = (id)=> $.get( "https://swapi.co/api/films/"+id+"/", (data)=> console.log(data));
+let getFilmData = (id)=> $.get( "https://swapi.co/api/films/"+id+"/", (data)=> buildHtmlElements(data));
 //let buildHtmlElements = (responseData) => 
 console.log("here");
-function getFilmData(id) {
+/*function getFilmData(id) {
   $.get( "https://swapi.co/api/films/"+id+"/", function( data ) {
   	console.log(data);
   	buildHtmlElements(data);
 });
-}
+}*/
 function buildHtmlElements(responseData){
 	var title = responseData.title;
   	var director = responseData.director;
@@ -14,6 +14,10 @@ function buildHtmlElements(responseData){
   	var producer = responseData.producer;
   	var opening_crawl = responseData.opening_crawl;
   	var release_date = responseData.release_date;
-  	$("#films").append("<div class="film">");
-  	$( "p" ).addClass( "myClass yourClass" );
+  	$('<h2 class="film-title">'+title+'</h2>').appendTo($("#films"));
+  	$('<p class="film-director">'+director+'</p>').appendTo($("#films"));
+  	$('<p class="film-episode">'+episode+'</p>').appendTo($("#films"));
+  	$('<p class="film-producer">'+producer+'</p>').appendTo($("#films"));
+  	$('<p class="film-opening_crawl">'+opening_crawl+'</p>').appendTo($("#films"));
+  	$('<p class="film-release_date">'+release_date+'</p>').appendTo($("#films"));
 }
