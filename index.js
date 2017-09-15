af7f7b7ae461d5fe8b1b677bf92192fb02a799ca
Sen2017 <casintahan47at@gmail.com>
2017-09-15 10:50:43 -1000

    bolded titles

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function buildHtmlElements(responseData){
   	var opening_crawl = responseData.opening_crawl;
   	var release_date = responseData.release_date;
   	$('<h2 class="film-title">'+title+'</h2>').appendTo($("#films"));
-  	$('<p class="film-director">Directed by: '+director+'</p>').appendTo($("#films"));
-  	$('<p class="film-episode">Episode #'+episode+'</p>').appendTo($("#films"));
-  	$('<p class="film-producer">Produced by: '+producer+'</p>').appendTo($("#films"));
-  	$('<p class="film-opening_crawl"> Description:'+opening_crawl+'</p>').appendTo($("#films"));
-  	$('<p class="film-release_date">Relase date:'+release_date+'</p>').appendTo($("#films"));
+  	$('<p class="film-director"><b>Directed by:</b> '+director+'</p>').appendTo($("#films"));
+  	$('<p class="film-episode"><b>Episode:</b>'+episode+'</p>').appendTo($("#films"));
+  	$('<p class="film-producer"><b>Produced by:</b> '+producer+'</p>').appendTo($("#films"));
+  	$('<p class="film-opening_crawl"><b>Description:</b> '+opening_crawl+'</p>').appendTo($("#films"));
+  	$('<p class="film-release_date"><b>Relase date:</b>'+release_date+'</p>').appendTo($("#films"));
 }
