f5df74a478e42d1746cbad277541a32d62456010
Yoscroll <ianhenry.c808@gmail.com>
2017-09-15 18:30:47 -1000

    working on es6

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 let getFilmData = (id)=> $.get( "https://swapi.co/api/films/"+id+"/", (data)=> buildHtmlElements(data) );
-//let buildHtmlElements = (responseData) => 
 console.log("here");
-/*function getFilmData(id) {
-  $.get( "https://swapi.co/api/films/"+id+"/", function( data ) {
-  	console.log(data);
-  	buildHtmlElements(data);
-});
+var keys = [["title",""],["director","Directed by:"],["episode_id","Episode:"],["producer","Produced by:"],["opening_crawl","Description:"],["release_date","Relase date:"]];
+/*function buildHtmlElements(responseData){
+   var pep = Object.entries(responseData).map(function(x){
+      keys.map(function(y){
+        if (y[0].includes(x[0])){
+          if (x[0]==="title") {
+            $('<h2 class="film-title">'+x[1]+'</h2>').appendTo($("#films"));
+          }
+          else{
+            $('<p class="film-'+x[0]+'"><b>'+y[1]+'</b> '+x[1]+'</p>').appendTo($("#films"));
+          }
+        }
+      });
+   });
 }*/
-function buildHtmlElements(responseData){
-	var title = responseData.title;
-  	var director = responseData.director;
-  	var episode = responseData.episode_id;
-  	var producer = responseData.producer;
-  	var opening_crawl = responseData.opening_crawl;
-  	var release_date = responseData.release_date;
-  	$('<h2 class="film-title">'+title+'</h2>').appendTo($("#films"));
-  	$('<p class="film-director"><b>Directed by:</b> '+director+'</p>').appendTo($("#films"));
-  	$('<p class="film-episode"><b>Episode:</b>'+episode+'</p>').appendTo($("#films"));
-  	$('<p class="film-producer"><b>Produced by:</b> '+producer+'</p>').appendTo($("#films"));
-  	$('<p class="film-opening_crawl"><b>Description:</b> '+opening_crawl+'</p>').appendTo($("#films"));
-  	$('<p class="film-release_date"><b>Relase date:</b>'+release_date+'</p>').appendTo($("#films"));
-}
+let buildHtmlElements = (responseData) => Object.entries(responseData).map((x)=>keys.map((y)=>y[0].includes(x[0]) ? (x[0]==="title" ? $('<h2 class="film-title">'+x[1]+'</h2>').appendTo($("#films")):$('<p class="film-'+x[0]+'"><b>'+y[1]+'</b> '+x[1]+'</p>').appendTo($("#films")) ))
+let buildHtmlElements = (responseData) => Object.entries(responseData).map((x)=>keys.map((y)=>y[0].includes(x[0]) ? (x[0]==="title" ?console.log("ti") :console.log("not")):console.log("end no")));
