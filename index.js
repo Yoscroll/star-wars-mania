4c268cf8b54eae4fb890b6fb243fd6d5b8833fef
Yoscroll <ianhenry.c808@gmail.com>
2017-09-15 20:43:44 -1000

    finished chagning to es6

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 let getFilmData = (id)=> $.get( "https://swapi.co/api/films/"+id+"/", (data)=> buildHtmlElements(data) );
 console.log("here");
-var keys = [["title",""],["director","Directed by:"],["episode_id","Episode:"],["producer","Produced by:"],["opening_crawl","Description:"],["release_date","Relase date:"]];
+let keys = [["title",""],
+["director","Directed by:"],
+["episode_id","Episode:"],
+["producer","Produced by:"],
+["opening_crawl","Description:"],
+["release_date","Relase date:"]];
 let buildHtmlElements = (responseData) => Object.entries(responseData).map((x)=>keys.map((y)=>y[0].includes(x[0]) ? (x[0]==="title" ? $('<h2 class="film-title">'+x[1]+'</h2>').appendTo($("#films")):$('<p class="film-'+x[0]+'"><b>'+y[1]+'</b> '+x[1]+'</p>').appendTo($("#films"))):null));
