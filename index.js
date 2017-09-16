82f04ac2c15762a0b93f407c72713486866362df
Yoscroll <ianhenry.c808@gmail.com>
2017-09-15 20:42:43 -1000

    finished chagning to es6

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 let getFilmData = (id)=> $.get( "https://swapi.co/api/films/"+id+"/", (data)=> buildHtmlElements(data) );
 console.log("here");
 var keys = [["title",""],["director","Directed by:"],["episode_id","Episode:"],["producer","Produced by:"],["opening_crawl","Description:"],["release_date","Relase date:"]];
-/*function buildHtmlElements(responseData){
-   var pep = Object.entries(responseData).map(function(x){
-      keys.map(function(y){
-        if (y[0].includes(x[0])){
-          if (x[0]==="title") {
-            $('<h2 class="film-title">'+x[1]+'</h2>').appendTo($("#films"));
-          }
-          else{
-            $('<p class="film-'+x[0]+'"><b>'+y[1]+'</b> '+x[1]+'</p>').appendTo($("#films"));
-          }
-        }
-      });
-   });
-}*/
-let buildHtmlElements = (responseData) => Object.entries(responseData).map((x)=>keys.map((y)=>y[0].includes(x[0]) ? (x[0]==="title" ? $('<h2 class="film-title">'+x[1]+'</h2>').appendTo($("#films")):$('<p class="film-'+x[0]+'"><b>'+y[1]+'</b> '+x[1]+'</p>').appendTo($("#films")) ))
-let buildHtmlElements = (responseData) => Object.entries(responseData).map((x)=>keys.map((y)=>y[0].includes(x[0]) ? (x[0]==="title" ?console.log("ti") :console.log("not")):console.log("end no")));
+let buildHtmlElements = (responseData) => Object.entries(responseData).map((x)=>keys.map((y)=>y[0].includes(x[0]) ? (x[0]==="title" ? $('<h2 class="film-title">'+x[1]+'</h2>').appendTo($("#films")):$('<p class="film-'+x[0]+'"><b>'+y[1]+'</b> '+x[1]+'</p>').appendTo($("#films"))):null));
